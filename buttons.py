c8e694aad6229cb924d778302763200b016f763c
Roman <Klimovich.r@mail.ru>
2025-11-05 20:51:05 +0500

    created a new bot, button file and main file

---
+++ buttons.py
@@ -0,0 +1,11 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from aiogram.types import Message, ContentType
+from aiogram.types import (
+    ReplyKeyboardMarkup, # Нужен для обычной клавиатуры
+    KeyboardButton, # Нужен для обычной клавиатуры
+    InlineKeyboardMarkup, # Для инлайн клавиатуры
+    InlineKeyboardButton, # Для инлайн клавиатуры
+    ReplyKeyboardRemove
+)
+
