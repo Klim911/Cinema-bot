4f5a1aa013939138221138e376288ba99412a33f
Roman <Klimovich.r@mail.ru>
2025-11-26 00:56:50 +0500

    processing hendlers 3

--- hendlers/hendlers_main.py
+++ hendlers/hendlers_main.py
@@ -5,6 +5,8 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup, default_state
 from aiogram.types import Message, ContentType
 
+from lexicon.lexicon import LEXICON
+
 # Главное меню
 # Создаём класс для группы состояний главного меню
 class ChoiceOfFilms(StatesGroup):         # Выбор фильмов
@@ -19,12 +21,25 @@ router = Router()
 # главного меню
 @router.message(CommandStart(), StateFilter(default_state))
 async def process_start_command(message: Message):
-    pass
+    await message.answer(text=LEXICON["/start"])
 
 """Обрабатываем команду help"""
-# Этот хэндлер будет срабатывать на команду /help в любых состояниях, кроме состояния по умолчанию и сообщать
+# Этот хэндлер будет срабатывать на команду /help в состоянии по умолчанию, кроме состояния по умолчанию и сообщать
 # что вы можете сейчас сделать
-@router.message(Command(commands="/help"), ~StateFilter(default_state))
+@router.message(Command(commands="/help"), StateFilter(default_state))
 async def process_help_command():
     pass
 
+# Этот хэндлер будет срабатывать на команду "/cancel" в любых состояниях, кроме состояния по умолчанию и выполнять
+# действие назад.
+@router.message(Command(commands="cancel"), ~StateFilter(default_state))
+async def process_cancel_command(massage: Message, state: FSMContext):
+    pass
+
+# Этот хэндлер будет срабатывать на команду "go" и переводить бота в состояние первого выбора в главном меню,
+# после отправки этой команды, для пользователя откроются кнопки
+@router.message((F.text == LEXICON["go"]), StateFilter(default_state))
+async def process_go_command(message: Message, state: FSMContext):
+    await message.answer(text=LEXICON["go"])
+    # Устанавливаем состояние первого выбора в главном меню
+    await state.set_state(ChoiceOfFilms.first_choice)
--- hendlers/hendlers_search.py
+++ hendlers/hendlers_search.py
@@ -1,7 +1,10 @@
 from aiogram import F, Router, types
+from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.state import State, StatesGroup, default_state
 from aiogram.types import Message, ContentType
+
+from hendlers_main import ChoiceOfFilms
 
 # Посик фильмов
 # Создаём класс дял группы состояний внутри кнопки поиска фильма
@@ -15,3 +18,42 @@ class Categories(StatesGroup):          # Категории
     select_sorting_likes = State()      # Состояние ожидания выбора сортировки по лайкам
 
 
+search_router = Router()
+
+# Этот хэендлер будет срабатывать, если нажата кнопка "Поиск фильма" и переводить в состояние
+# ожидания выбора года
+@search_router.message(StateFilter(ChoiceOfFilms.first_choice))
+async def process_select_year_command(state: FSMContext):
+    pass
+    # Устанавливаем состояние выбора года
+    await state.set_state(Categories.select_year)
+
+
+# Этот хэндлер будет срабатывать если выбран один из годов и переводить в состояние выбора жанра
+@search_router.message(StateFilter(Categories.select_year))
+async def process_select_genre_command(state: FSMContext):
+    pass
+    # Устанавливаем состояние выбора жанра
+    await state.set_state(Categories.select_genre)
+
+
+# Этот хэндлер будет срабатывать если выбран один из жанров и переводить в состояние выбора рейтинга
+@search_router.message(StateFilter(Categories.select_genre))
+async def process_select_genre_command(state: FSMContext):
+    pass
+    # Устанавливаем состояние выбора рейтинга
+    await state.set_state(Categories.select_rating)
+
+
+# Этот хэндлер будет срабатывать если выбран рейтинг и переводить в состояние выбора времени просмотра
+@search_router.message(StateFilter(Categories.select_rating))
+async def process_select_time_command(state: FSMContext):
+    pass
+    # Устанавливаем состояние выбора времени просмотра
+    await state.set_state(Categories.select_time)
+
+
+# Этот хэндлер будет срабатывать если выбрано время просмотра и переводить в ???
+@search_router.message(StateFilter(Categories.select_time))
+async def process_select_review_command(state: FSMContext):
+    pass
